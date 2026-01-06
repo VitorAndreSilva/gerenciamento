caca1e42c325b5eb61be76a18d4197d2509ee30d
VitorAndreSilva <alunovitorandre@gmail.com>
2026-01-06 12:19:38 -0300

    FIX: Corrige erro de CORS

--- gerenciador/config/settings.py
+++ gerenciador/config/settings.py
@@ -33,9 +33,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",,
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
